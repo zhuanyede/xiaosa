3d90d0d6b2c993496ea88040defcdc7cccd586c4
celin1286 <95584596+celin1286@users.noreply.github.com>
2025-02-25 12:26:41 +0800

    Update url.py

--- url.py
+++ url.py
@@ -23,7 +23,7 @@ site_mappings = {
     '下饭': 'xiafan',
     '玩偶': 'wogg',
     '星剧社': 'star2',
-    '小二': 'xiaoai'
+    '小二': 'xiaoer'
 }
 
 buye_mappings = {
@@ -40,7 +40,7 @@ buye_mappings = {
     '下饭': 'xiaf',
     '玩偶': 'wogg',
     '星剧社': 'star2',
-    '小二': 'xiaoai'
+    '小二': 'xiaoer'
 }
 
 def test_url(url):
